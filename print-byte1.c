d759e2fe1c37ff95451ea0c1d449265350ed2d38
kenny wu <kenny@homelander.cosi107a.org>
2025-02-10 18:48:00 -0500

    v1

--- print-byte1.c
+++ print-byte1.c
@@ -6,7 +6,7 @@ int main(int argc, char *argv[]) {
         int num = strtol(argv[i], NULL, 0);  
         int byte1 = (num >> 8) & 0xFF;       
 
-        printf("%d 0x%02X\n", i, byte1);
+        printf("0x%02X %d\n", byte1, byte1);
     }
     return 0;
 }
--- print-sizes.c
+++ print-sizes.c
@@ -6,16 +6,16 @@ int main() {
     char char_array[200];
     char *char_ptr;
 
-    printf("The size of char is %lu\n", sizeof(char));
-    printf("The size of short is %lu\n", sizeof(short));
-    printf("The size of int is %lu\n", sizeof(int));
-    printf("The size of long is %lu\n", sizeof(long));
-    printf("The size of long long is %lu\n", sizeof(long long));
-    printf("The size of float is %lu\n", sizeof(float));
-    printf("The size of double is %lu\n", sizeof(double));
-    printf("The size of char * is %lu\n", sizeof(char *));
-    printf("The size of int * is %lu\n", sizeof(int *));
-    printf("The size of long * is %lu\n", sizeof(long *));
+    printf("size of char is %lu\n", sizeof(char));
+    printf("size of short is %lu\n", sizeof(short));
+    printf("size of int is %lu\n", sizeof(int));
+    printf("size of long is %lu\n", sizeof(long));
+    printf("size of long long is %lu\n", sizeof(long long));
+    printf("size of float is %lu\n", sizeof(float));
+    printf("size of double is %lu\n", sizeof(double));
+    printf("size of char * is %lu\n", sizeof(char *));
+    printf("size of int * is %lu\n", sizeof(int *));
+    printf("size of long * is %lu\n", sizeof(long *));
 
     printf("The size of my character array is %lu\n", sizeof(char_array));
 
